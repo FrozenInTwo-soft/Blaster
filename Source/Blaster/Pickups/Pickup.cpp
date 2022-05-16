ca5e6f4c1c029be874dea602267065f57232b34a
FrozenInTwo <Christian.sterzinger@gmail.com>
2022-05-16 00:03:02 -0500

    Pickup Spawner
    Implemented a Spawner for pickups. This works for Ammo and Buffs, It uses an array to determine what it will spawn so it can be filled with multiple types to spawn a random pickup each time.

--- Source/Blaster/Pickups/Pickup.cpp
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -41,7 +41,12 @@ void APickup::BeginPlay()
 
 	if (HasAuthority())
 	{
-		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+		GetWorldTimerManager().SetTimer(
+			BindOverlapTimer,
+			this,
+			&APickup::BindOverlapTimerFinished,
+			BindOverlapTime
+		);
 	}
 }
 
@@ -49,6 +54,11 @@ void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* 
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	
+}
+
+void APickup::BindOverlapTimerFinished()
+{
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
 }
 
 void APickup::Tick(float DeltaTime)
