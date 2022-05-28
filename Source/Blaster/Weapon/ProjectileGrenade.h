e5793ca2a8fbef374ade982897bb25afc13bb552
FrozenInTwo <Christian.sterzinger@gmail.com>
2022-05-28 12:59:35 -0500

    Post Edit Changes
    implemented PostEditChangeProperty on the Projectile classes so we only need to change the speed in one location without going into the movement component.

--- Source/Blaster/Weapon/ProjectileGrenade.h
+++ Source/Blaster/Weapon/ProjectileGrenade.h
@@ -17,6 +17,10 @@ public:
 	AProjectileGrenade();
 	virtual void Destroyed() override;
 
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
+#endif
+
 protected:
 	virtual void BeginPlay() override;
 	
--- Source/Blaster/Weapon/ProjectileRocket.h
+++ Source/Blaster/Weapon/ProjectileRocket.h
@@ -15,12 +15,14 @@ class BLASTER_API AProjectileRocket : public AProjectile
 	GENERATED_BODY()
 
 public:
-
 	AProjectileRocket();
 	virtual void Destroyed() override;
-	
-protected:
 
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
+#endif
+
+protected:
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		FVector NormalImpulse, const FHitResult& Hit) override;
 	virtual void BeginPlay() override;
